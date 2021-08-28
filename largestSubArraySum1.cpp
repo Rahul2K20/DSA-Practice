0c506702033f6853f8e5e4e1f12685b19f4b7e1e
Rahul2K20 <63788480+Rahul2K20@users.noreply.github.com>
2021-08-28 12:53:28 -0700

    Add files via upload

---
+++ largestSubArraySum1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int largestSubArraySum(int arr[], int n) {
+  int largestSum = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j < n; j++) {
+      int currentSum = 0;
+      for (int k = i; k <= j; k++) {
+
+        currentSum += arr[k];
+      }
+      largestSum = max(largestSum, currentSum);
+    }
+  }
+  return largestSum;
+
+}
+
+int main() {
+  int arr[] = {-2,3,4,-1,5,-12,6,1,3};
+  int n = sizeof(arr) / sizeof(int);
+  cout << largestSubArraySum(arr, n);
+}
