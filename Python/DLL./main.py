8625535d7989cedd3f2f0115fdba8a3998381bd9
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2023-09-03 09:50:19 -0700

    Update main.py

--- Python/DLL./main.py
+++ Python/DLL./main.py
@@ -149,19 +149,3 @@ my_doubly_linked_list.append(6)
 my_doubly_linked_list.remove(1)
 
 my_doubly_linked_list.print_list()
-
-# print('Head:', my_doubly_linked_list.head.value)
-# print('Tail:', my_doubly_linked_list.tail.value)
-# print('Length:', my_doubly_linked_list.length)
-
-
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Head: 7
-    Tail: 7
-    Length: 1
-
-"""
